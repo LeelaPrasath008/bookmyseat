46ed4026ae37f0a6ac07ac6c1e6960036a24d71a
Leelaprasath V <rubendhiran2014@gmail.com>
2026-09-08 02:19:04 +0530

    Fix Render configuration

--- bookmyseat/settings.py
+++ bookmyseat/settings.py
@@ -39,13 +39,14 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'bookmyseat.urls'
@@ -73,7 +74,7 @@ WSGI_APPLICATION = 'bookmyseat.wsgi.application'
 
 DATABASES = {
     "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL")
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
     )
 }
 
