a21f06db1ad146f901c7bd0e8c28ba8a1762ce67
Leelaprasath V <rubendhiran2014@gmail.com>
2026-09-08 01:26:11 +0530

    Add PostgreSQL support

--- bookmyseat/settings.py
+++ bookmyseat/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 import os
 from pathlib import Path
-
+import dj_database_url
 from dotenv import load_dotenv
 
 # Loads variables from a local .env file (never committed to git) into
@@ -83,10 +83,9 @@ WSGI_APPLICATION = 'bookmyseat.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.parse(
+        os.environ.get("DATABASE_URL")
+    )
 }
 
 # Password validation
