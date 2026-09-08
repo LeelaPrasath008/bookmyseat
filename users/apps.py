bdaaa271f7571ff7898672a841e111f1b0920a14
Leelaprasath V <rubendhiran2014@gmail.com>
2026-09-08 17:32:04 +0530

    Added movie management updates and responsive design fixes

--- users/apps.py
+++ users/apps.py
@@ -9,3 +9,14 @@ class UsersConfig(AppConfig):
 
     def ready(self):
         import users.signals
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser(
+        username="admin",
+        email="admin@example.com",
+        password="Admin@123"
+    )
