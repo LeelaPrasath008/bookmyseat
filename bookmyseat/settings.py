474798447f7c3ea3d67c99d6d28ce8d309ca54b4
Leelaprasath V <rubendhiran2014@gmail.com>
2026-09-08 17:45:52 +0530

    Configure PostgreSQL database

--- bookmyseat/settings.py
+++ bookmyseat/settings.py
@@ -74,7 +74,7 @@ WSGI_APPLICATION = 'bookmyseat.wsgi.application'
 
 DATABASES = {
     "default": dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
+        default=os.environ.get("DATABASE_URL")
     )
 }
 
