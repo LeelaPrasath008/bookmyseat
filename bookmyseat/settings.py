71ce6ad2024642939c814720817628d6322880d1
Leelaprasath V <rubendhiran2014@gmail.com>
2026-09-08 23:17:21 +0530

    Fix Cloudinary static storage configuration

--- bookmyseat/settings.py
+++ bookmyseat/settings.py
@@ -168,6 +168,7 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
